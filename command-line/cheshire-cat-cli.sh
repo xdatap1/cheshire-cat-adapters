cb675c9ff193a57c816e873c67a6b14ceb35cc87
Paolo Sammicheli <paolo@sammiche.li>
2023-05-11 15:15:24 +0200

    On line help improvements

--- command-line/cheshire-cat-cli.sh
+++ command-line/cheshire-cat-cli.sh
@@ -2,10 +2,19 @@
 
 # Define usage message
 usage() {
+  echo ""
   echo "Usage: $0 [-h] TEXT"
-  echo "Send TEXT to cheshire cat websocket located in ws://localhost:1865/ws, extract the 'content' field from the JSON response, and return it."
+  echo "Send TEXT to cheshire cat websocket located in ws://localhost:1865/ws, extract the 'content' field from the JSON response, and return it. It works also with pipeline from other commands."
+  echo ""
   echo "Options:"
   echo "  -h, --help    Show this help message and exit"
+  echo ""
+  echo "Examples:"
+  echo "./cheshire-cat-cli.sh Hello my friend"
+  echo ""
+  echo "echo 'Hello my friend' | ./cheshire-cat-cli.sh"
+  echo ""
+  
 }
 
 # Parse command line options
@@ -25,6 +34,7 @@ done
 
 # Read from pipe if no argument was provided
 if [ -t 0 ] && [ -z "$text" ]; then
+  echo ""
   echo "Error: No text provided"
   usage
   exit 1
